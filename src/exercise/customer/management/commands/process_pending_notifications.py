ed9511e8c90de1a8aa92067061faf0a5f78a5fa2
DESKTOP-AGVAP3M\Andrea <andreagarciasinisterra@gmail.com>
2022-03-08 08:15:04 +0100

    Add threads in command

--- src/exercise/customer/management/commands/process_pending_notifications.py
+++ src/exercise/customer/management/commands/process_pending_notifications.py
@@ -1,8 +1,8 @@
 import urllib.request
 import json_stream
 
+from concurrent.futures import ThreadPoolExecutor
 from django.core.management import BaseCommand
-
 from django.conf import settings
 from exercise.customer.actions import customer_notification
 from exercise.customer.models import Customer
@@ -14,13 +14,13 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         with urllib.request.urlopen(settings.NOTIFICATIONS_LOG_URL) as response:
             data = json_stream.load(response, persistent=False)
-
-            for item in data:
-                customer = Customer(
-                    name=item['name'],
-                    email=item['email'],
-                    phone=item['phone'],
-                    url=item['url'],
-                    type=item['type']
-                )
-                customer_notification.execute(customer)
+            with ThreadPoolExecutor(max_workers=settings.MAX_WORKERS) as thread:
+                for item in data:
+                    customer = Customer(
+                        name=item['name'],
+                        email=item['email'],
+                        phone=item['phone'],
+                        url=item['url'],
+                        type=item['type']
+                    )
+                    thread.submit(customer_notification.execute, customer)
--- src/exercise/settings/base.py
+++ src/exercise/settings/base.py
@@ -1,3 +1,5 @@
 from .django import *
 
 NOTIFICATIONS_LOG_URL = 'https://raw.githubusercontent.com/UN-ICC/notifications-processor/master/notifications_log.json'
+
+MAX_WORKERS = 4
